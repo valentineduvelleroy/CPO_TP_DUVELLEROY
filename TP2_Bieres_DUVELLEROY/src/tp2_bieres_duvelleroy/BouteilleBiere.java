de0be3bd387e3efbe135774ae4a2b05ba4e945eb
valen <valen@LAPTOP-PM0POMS3.education.lakanal.epf.fr>
2022-10-25 16:59:11 +0200

    Création du booléen décapsuler (page 4)

---
+++ TP2_Bieres_DUVELLEROY/src/tp2_bieres_duvelleroy/BouteilleBiere.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_duvelleroy;
+
+/**
+ *
+ * @author valen
+ */
+ public class BouteilleBiere {
+
+    public String nom;
+    public float degreAlcool;
+    public String brasserie;
+    public boolean ouverte;
+
+    public void lireEtiquette() {
+        System.out.println("Bouteille de " + nom + " (" + degreAlcool + " degres) \nBrasserie : " + brasserie);
+
+    }
+
+    public BouteilleBiere(String unNom, float unDegre, String uneBrasserie) {
+        nom = unNom;
+        degreAlcool = unDegre;
+        brasserie = uneBrasserie;
+        ouverte = false;
+        
+    public boolean Decapsuler (); {
+        if (ouverte == false) {
+     Decapsuler = true
+ }
+        else {
+     Decapsuler = true
+ }
+    
+    }
+}
