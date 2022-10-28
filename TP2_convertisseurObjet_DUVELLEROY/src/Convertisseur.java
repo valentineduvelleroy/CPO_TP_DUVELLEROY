e407046c13e766041a93d9b7e1cd3f841d3f1fcd
valen <valen@LAPTOP-PM0POMS3.education.lakanal.epf.fr>
2022-10-28 17:06:10 +0200

    Partie 2 création des attributs et méthodes

---
+++ TP2_convertisseurObjet_DUVELLEROY/src/Convertisseur.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author valen
+ */
+public class Convertisseur {
+        int nbConversions = 0;
+    int nb = nbConversions;
+   
+public double CelsiusVersKelvin ( double TC ){
+    double TK = TC + 273.14;
+        return TK;
+}
+
+public double KelvinVersCelsius ( double TK ){
+    double TC = TK - 273.14;
+        return TC;
+}
+
+public double FarenheitVersCelsius ( double TF ){
+    double TC = (TF - 32) / 1.8;
+        return TC;
+}
+
+public double CelsiusVersFarenheit ( double TC ){
+    double TF = ()
+        return TF
+}
+    
+}
+
+
