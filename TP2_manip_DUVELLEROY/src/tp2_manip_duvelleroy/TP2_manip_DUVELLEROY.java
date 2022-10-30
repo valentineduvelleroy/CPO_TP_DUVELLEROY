e208687eb65b0e48d26408567793b2b8b7517e44
valen <valen@LAPTOP-PM0POMS3.education.lakanal.epf.fr>
2022-10-30 21:03:47 +0100

    ajout des commentaires dans le main manip + ajout de la question 5 que j'avais loupée

--- TP2_manip_DUVELLEROY/src/tp2_manip_duvelleroy/TP2_manip_DUVELLEROY.java
+++ TP2_manip_DUVELLEROY/src/tp2_manip_duvelleroy/TP2_manip_DUVELLEROY.java
@@ -17,16 +17,24 @@ public class TP2_manip_DUVELLEROY {
     Tartiflette assiette1 = new Tartiflette(500);
     Tartiflette assiette2 = new Tartiflette(600);
  
-        Tartiflette assiette3 = assiette2; 
+        Tartiflette assiette3 = assiette2; //Il y en a réellement 1 seule qui a été créée, 
+        //car nbCalories de 3 = nbCalories de 2 = 600 qui correspond à une tartiflette 
+        //Elles représentent la même tartiflette
+       
+        assiette2 = assiette1;
+        assiette1 = assiette3; 
+        //Echange les objets assiette 1 et assiette 2 grâce à une autre variable
         
         System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories) ;
         System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories) ;
 
+       //Les deux lignes de codes ne fonctionnent pas
        //Moussaka assiette666 = assiette1 ;
        //Moussaka assiette667 = new Tartiflette() ;
       
        Moussaka tab [] = new Moussaka [10];
-        for (int i = 0; i < 9; i++){
+        for (int i = 0; i < 9; i++){ 
+        //Strictement inférieur à 9 car le tableau commence à 0, donc il y a bien 10 valeurs
             Moussaka T = new Moussaka();
             tab[i]=T;
             System.out.println(tab[i]);
